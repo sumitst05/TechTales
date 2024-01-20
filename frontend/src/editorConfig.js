75ad9158420ecab58d5c3639d32f6be0156048b5
sumitst05 <sumitst05@gmail.com>
2024-01-20 14:23:30 +0530

    Separated editor configurations

---
+++ frontend/src/editorConfig.js
@@ -0,0 +1,31 @@
+const editorStyle = {
+  width: "80%",
+  border: "1px solid #ccc",
+  borderRadius: "8px",
+};
+
+const modules = {
+  toolbar: [
+    ["bold", "italic", "underline", "strike"],
+    [{ header: 1 }, { header: 2 }],
+    [{ list: "ordered" }, { list: "bullet" }],
+    [{ align: [] }],
+    ["link"],
+    ["image"],
+  ],
+};
+
+const formats = [
+  "header",
+  "bold",
+  "italic",
+  "underline",
+  "strike",
+  "list",
+  "align",
+  "bullet",
+  "link",
+  "image",
+];
+
+export { editorStyle, modules, formats };
