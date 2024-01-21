d576fc7468e67a868b47dce7c4e4fe66141566a1
sumitst05 <sumitst05@gmail.com>
2024-01-21 15:07:12 +0530

    updates

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://techtales-4ap2.onrender.com",
       },
     },
   },
