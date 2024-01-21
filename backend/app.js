0854320024293dcad088a6c422e1981dcf2c39db
sumitst05 <sumitst05@gmail.com>
2024-01-21 11:38:00 +0530

    updates

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
 	cors({
-		origin: "http://localhost:5173",
+		origin: "https://tech-tales-frontend.vercel.app/",
 		credentials: true,
 	}),
 );
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000/",
+        target: "https://tech-tales-backend.vercel.app/",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
