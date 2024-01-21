fa00b54ab0c2140578c3603601e9c7ac3217b0e6
sumitst05 <sumitst05@gmail.com>
2024-01-21 11:50:14 +0530

    updates

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": {
+      "/backend": {
         target: "https://tech-tales-backend.vercel.app/",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
