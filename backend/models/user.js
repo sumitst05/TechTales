f0b80793c15041d67476f5770d59d573daef4f3b
sumitst05 <sumitst05@gmail.com>
2024-02-04 14:04:20 +0530

    added: bookmarking articles

--- backend/models/user.js
+++ backend/models/user.js
@@ -2,38 +2,42 @@ import mongoose from "mongoose";
 import validator from "validator";
 
 const userSchema = new mongoose.Schema(
-  {
-    username: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      validate: {
-        validator: validator.isEmail,
-        message: "Please provide a valid email!",
-      },
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    profilePicture: {
-      type: String,
-      default: "https://cdn-icons-png.freepik.com/128/3237/3237472.png",
-    },
-    likedArticles: {
-      type: [mongoose.Schema.Types.ObjectId],
-      ref: "Article",
-    },
-    accessToken: {
-      type: String,
-    },
-  },
-  { timestamps: true },
+	{
+		username: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+			validate: {
+				validator: validator.isEmail,
+				message: "Please provide a valid email!",
+			},
+		},
+		password: {
+			type: String,
+			required: true,
+		},
+		profilePicture: {
+			type: String,
+			default: "https://cdn-icons-png.freepik.com/128/3237/3237472.png",
+		},
+		likedArticles: {
+			type: [mongoose.Schema.Types.ObjectId],
+			ref: "Article",
+		},
+		bookmarkedArticles: {
+			type: [mongoose.Schema.Types.ObjectId],
+			ref: "Article",
+		},
+		accessToken: {
+			type: String,
+		},
+	},
+	{ timestamps: true },
 );
 
 const User = mongoose.model("User", userSchema);
--- frontend/src/components/ArticleCard.jsx
+++ frontend/src/components/ArticleCard.jsx
@@ -2,93 +2,137 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 import {
-  updateUserFailure,
-  updateUserStart,
-  updateUserSuccess,
+	updateUserFailure,
+	updateUserStart,
+	updateUserSuccess,
 } from "../redux/user/userSlice";
 
 function ArticleCard({ article }) {
-  const [likedStatus, setLikedStatus] = useState(false);
+	const [likedStatus, setLikedStatus] = useState(false);
+	const [bookmarkedStatus, setBookmarkedStatus] = useState(false);
 
-  const { currentUser } = useSelector((state) => state.user);
-  const dispatch = useDispatch();
+	const { currentUser } = useSelector((state) => state.user);
+	const dispatch = useDispatch();
 
-  useEffect(() => {
-    const likedArticleIdsSet = new Set(currentUser.likedArticles || []);
-    setLikedStatus(likedArticleIdsSet.has(article._id));
-  }, [article._id, currentUser.likedArticles]);
+	useEffect(() => {
+		const likedArticleIdsSet = new Set(currentUser.likedArticles || []);
+		setLikedStatus(likedArticleIdsSet.has(article._id));
 
-  const handleLike = async () => {
-    const likeCount = article.likes + (likedStatus ? -1 : 1);
+		const bookmarkedArticleIdsSet = new Set(
+			currentUser.bookmarkedArticles || [],
+		);
+		setBookmarkedStatus(bookmarkedArticleIdsSet.has(article._id));
+	}, [article._id, currentUser.likedArticles, currentUser.bookmarkedArticles]);
 
-    try {
-      dispatch(updateUserStart());
+	async function handleLike() {
+		const likeCount = article.likes + (likedStatus ? -1 : 1);
 
-      const isAlreadyLiked = likedStatus;
+		try {
+			dispatch(updateUserStart());
 
-      const updatedLikedArticles = new Set(currentUser.likedArticles);
+			const isAlreadyLiked = likedStatus;
 
-      if (isAlreadyLiked) {
-        updatedLikedArticles.delete(article._id);
-      } else {
-        updatedLikedArticles.add(article._id);
-      }
+			const updatedLikedArticles = new Set(currentUser.likedArticles);
 
-      const res = await axios.patch(`/api/user/${currentUser._id}`, {
-        ...currentUser,
-        likedArticles: Array.from(updatedLikedArticles),
-      });
+			if (isAlreadyLiked) {
+				updatedLikedArticles.delete(article._id);
+			} else {
+				updatedLikedArticles.add(article._id);
+			}
 
-      const data = res.data;
+			const res = await axios.patch(`/api/user/${currentUser._id}`, {
+				...currentUser,
+				likedArticles: Array.from(updatedLikedArticles),
+			});
 
-      await axios.patch(`/api/articles/${article._id}`, { likes: likeCount });
+			const data = res.data;
 
-      dispatch(updateUserSuccess(data));
-    } catch (error) {
-      error.message = error.response
-        ? error.response.data.message
-        : error.response.statusText;
-      dispatch(updateUserFailure(error.message));
-    }
+			await axios.patch(`/api/articles/${article._id}`, { likes: likeCount });
 
-    setLikedStatus(!likedStatus);
-  };
+			dispatch(updateUserSuccess(data));
+		} catch (error) {
+			error.message = error.response
+				? error.response.data.message
+				: error.response.statusText;
+			dispatch(updateUserFailure(error.message));
+		}
 
-  return (
-    <div
-      className={`flex items-center text-slate-700 hover:scale-105 hover:text-slate-200 bg-slate-200 hover:bg-gradient-to-r from-violet-600 to-indigo-400 rounded-lg px-4 py-4 pb-2 gap-6 w-full`}
-    >
-      <div className="flex flex-col justify-between 2 mx-2 w-full line-clamp-1">
-        <div className="flex gap-2 items-center">
-          <img
-            src={article.author?.profilePicture}
-            alt="profile"
-            className="h-6 w-6 rounded-full"
-          />
-          <p className="font-medium overflow-hidden overflow-ellipsis">
-            {article.author ? article.author.username : "Unknown"}
-          </p>
-        </div>
-        <p className="text-2xl font-bold overflow-hidden overflow-ellipsis">
-          {article.title}
-        </p>
-        <div className="flex mt-2 items-center gap-1">
-          <img
-            src={likedStatus ? "/liked.png" : "/like.png"}
-            alt="like"
-            className="rounded-full h-5 w-5"
-            onClick={handleLike}
-          />
-          <p className="font-medium">{article.likes}</p>
-        </div>
-      </div>
-      <img
-        src={article.coverImage}
-        alt="cover-image"
-        className="h-16 w-16 flex-shrink-0 rounded-lg bg-slate-200"
-      />
-    </div>
-  );
+		setLikedStatus(!likedStatus);
+	}
+
+	async function handleBookmark() {
+		try {
+			dispatch(updateUserStart());
+
+			const isAlreadyBookmarked = bookmarkedStatus;
+			const updatedBookmarkedArticles = new Set(currentUser.bookmarkedArticles);
+
+			if (isAlreadyBookmarked) {
+				updatedBookmarkedArticles.delete(article._id);
+			} else {
+				updatedBookmarkedArticles.add(article._id);
+			}
+
+			const res = await axios.patch(`/api/user/${currentUser._id}`, {
+				...currentUser,
+				bookmarkedArticles: Array.from(updatedBookmarkedArticles),
+			});
+			const data = res.data;
+
+			dispatch(updateUserSuccess(data));
+		} catch (error) {
+			error.message = error.response
+				? error.response.data.message
+				: error.response.statusText;
+			dispatch(updateUserFailure(error.message));
+		}
+
+		setBookmarkedStatus(!bookmarkedStatus);
+	}
+
+	return (
+		<div
+			className={`flex items-center text-slate-700 hover:scale-105 hover:text-slate-200 bg-slate-200 hover:bg-gradient-to-r from-violet-600 to-indigo-400 rounded-lg px-4 py-4 pb-2 gap-6 w-full`}
+		>
+			<div className="flex flex-col justify-between 2 mx-2 w-full line-clamp-1">
+				<div className="flex gap-2 items-center">
+					<img
+						src={article.author?.profilePicture}
+						alt="profile"
+						className="h-6 w-6 rounded-full"
+					/>
+					<p className="font-medium overflow-hidden overflow-ellipsis">
+						{article.author ? article.author.username : "Unknown"}
+					</p>
+				</div>
+				<p className="text-2xl font-bold overflow-hidden overflow-ellipsis">
+					{article.title}
+				</p>
+				<div className="flex mt-2 items-center gap-4">
+					<div className="flex items-center gap-1">
+						<img
+							src={likedStatus ? "/liked.png" : "/like.png"}
+							alt="like"
+							className="rounded-full h-5 w-5"
+							onClick={handleLike}
+						/>
+						<p className="font-medium">{article.likes}</p>
+					</div>
+					<img
+						src={bookmarkedStatus ? "/bookmarked.png" : "/bookmark.png"}
+						alt="bookmark"
+						className="rounded-full h-5 w-5"
+						onClick={handleBookmark}
+					/>
+				</div>
+			</div>
+			<img
+				src={article.coverImage}
+				alt="cover-image"
+				className="h-16 w-16 flex-shrink-0 rounded-lg bg-slate-200"
+			/>
+		</div>
+	);
 }
 
 export default ArticleCard;
