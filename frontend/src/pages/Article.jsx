dbb1ae863ed345d73ef3c816ccd3ae85983c92b9
sumitst05 <sumitst05@gmail.com>
2024-02-12 22:35:23 +0530

    feat(Article.jsx): Added feature to display 5 tags and toggle functinality

--- frontend/src/pages/Article.jsx
+++ frontend/src/pages/Article.jsx
@@ -7,6 +7,7 @@ function Article() {
   const { articleId } = useParams();
   const [article, setArticle] = useState({});
   const [readTime, setReadTime] = useState(0);
+  const [showAllTags, setShowAllTags] = useState(false);
 
   useEffect(() => {
     const fetchArticle = async () => {
@@ -27,12 +28,16 @@ function Article() {
     fetchArticle();
   }, []);
 
+  function handleToggleTags() {
+    setShowAllTags(!showAllTags);
+  }
+
   return (
     <div className="article flex flex-col mt-16 max-w-3xl mx-auto p-2 gap-4">
       <p className="mt-6 font-serif font-bold text-4xl text-slate-900">
         {article.title}
       </p>
-      <div className="flex items-center gap-1">
+      <div className="flex items-center gap-1 select-none">
         <img
           src={article.author?.profilePicture}
           alt="profile"
@@ -43,9 +48,38 @@ function Article() {
         </p>
         <p className="font-light mt-1">•</p>
         <p className="text-slate-700 text-sm font-normal mt-1">
-          {readTime + " "}
-          {readTime > 1 ? "minutes read." : "minute read."}
+          {" " + readTime + " "}
+          {readTime > 1 ? "minutes read" : "minute read"}
         </p>
+      </div>
+
+      <div className="flex flex-wrap items-center w-full gap-4 overflow-x-auto">
+        {showAllTags
+          ? article.tags &&
+            article.tags.map((tag, index) => (
+              <div key={index}>
+                <p className="px-2 py-1 bg-slate-200 text-slate-600 text-sm rounded-lg">
+                  {tag}
+                </p>
+              </div>
+            ))
+          : article.tags &&
+            article.tags.slice(0, 5).map((tag, index) => (
+              <div key={index}>
+                <p className="px-2 py-1 bg-slate-200 text-slate-600 text-sm rounded-lg">
+                  {tag}
+                </p>
+              </div>
+            ))}
+
+        {article.tags && article.tags.length > 5 && (
+          <span
+            className="text-sm text-slate-600 -mx-2 cursor-pointer"
+            onClick={handleToggleTags}
+          >
+            {showAllTags ? "...less" : "...more"}
+          </span>
+        )}
       </div>
 
       <div className="flex justify-center items-center font-serif text-justify text-xl">
