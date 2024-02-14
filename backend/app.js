4ddc69b6eb81d7faa5b58ae3daac6e09e48da63e
sumitst05 <sumitst05@gmail.com>
2024-02-14 20:32:05 +0530

    chore: added firebase url in cors

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
 	cors({
-		origin: "http://localhost:5173",
+		origin: ["http://localhost:5173", "https://techtales-564.firebaseapp.com"],
 		credentials: true,
 	}),
 );
