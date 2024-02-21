1390696d3af0038cf9572972dce67d38c840557f
sumitst05 <sumitst05@gmail.com>
2024-02-21 19:45:50 +0530

    updated: articles grid style

--- frontend/src/pages/LikedArticles.jsx
+++ frontend/src/pages/LikedArticles.jsx
@@ -11,7 +11,7 @@ function LikedArticles() {
   const [page, setPage] = useState(
     parseInt(localStorage.getItem("likedArticlesPage")) || 1,
   );
-  const [pageSize, setPageSize] = useState(6);
+  const [pageSize, setPageSize] = useState(12);
   const [totalPages, setTotalPages] = useState(0);
   const [loading, setLoading] = useState(false);
 
@@ -47,24 +47,24 @@ function LikedArticles() {
   }, [page]);
 
   return (
-    <div className="flex flex-col justify-between mt-16 max-w-6xl mx-auto p-3 select-none overflow-hidden">
-      <h1 className="text-4xl text-center font-semibold p-3 text-transparent bg-clip-text bg-gradient-to-r from-indigo-400 to-violet-800">
+    <div className="flex flex-col justify-between mt-16 max-w-6xl mx-auto p-3 gap-4 select-none overflow-hidden">
+      <h1 className="text-4xl text-center font-semibold p-3 mb-2 text-transparent bg-clip-text bg-gradient-to-r from-indigo-400 to-violet-800">
         Liked Articles
       </h1>
-      <div className="flex justify-center mx-auto w-full mt-2">
-        <div className="relative w-full md:max-w-screen-2xl grid grid-cols-1 md:grid-cols-2 gap-4">
-          {loading && <Loader />}
-          {!loading &&
-            articles.map((article) => (
-              <ArticleCard key={article._id} article={article} />
-            ))}
-        </div>
+
+      <div className="flex-grow w-full md:max-w-screen-2xl grid grid-cols-1 lg:grid-cols-2 gap-4">
+        {loading && <Loader />}
+        {!loading &&
+          articles.map((article) => (
+            <ArticleCard key={article._id} article={article} />
+          ))}
       </div>
 
       <Pagination
         currentPage={page}
         totalPages={totalPages}
         onPageChange={(newPage) => setPage(newPage)}
+        loading={loading}
       />
     </div>
   );
