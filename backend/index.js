0078c555b4b345307d3afd8b0ababdadd0ed514f
sumitst05 <sumitst05@gmail.com>
2024-02-24 14:46:04 +0530

    deploying backend on vercel

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,6 @@ import mongoose from "mongoose";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import "dotenv/config";
-import path from "path";
 
 const app = express();
 
@@ -14,35 +13,31 @@ import articleRouter from "./routes/articles.js";
 import authRouter from "./routes/auth.js";
 import userRouter from "./routes/users.js";
 
-const __dirname = path.resolve();
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
-	cors({
-		origin: ["http://localhost:5173", "https://techtales-564.firebaseapp.com"],
-		credentials: true,
-	}),
+  cors({
+    origin: ["http://localhost:5173", "https://techtales-564.firebaseapp.com"],
+    credentials: true,
+  }),
 );
 app.use(cookieParser());
 
+app.get("/", (req, res) => {
+  res.json({ message: "Hello" });
+});
 app.use("/api", indexRouter);
 app.use("/api/articles", articleRouter);
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
-app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-});
-
 mongoose
-	.connect(process.env.DB_URL)
-	.then(
-		app.listen(port, () => {
-			console.log(`Listening on port ${port}...`);
-		}),
-	)
-	.catch((error) =>
-		console.log(`Error while connecting to database: ${error}`),
-	);
+  .connect(process.env.DB_URL)
+  .then(
+    app.listen(port, () => {
+      console.log(`Listening on port ${port}...`);
+    }),
+  )
+  .catch((error) =>
+    console.log(`Error while connecting to database: ${error}`),
+  )
