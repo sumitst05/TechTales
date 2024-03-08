ce45f20384e7ace76e25d67ad975672d5f0cd421
sumitst05 <sumitst05@gmail.com>
2024-03-08 17:41:25 +0530

    feat: added item selection in search bar

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect, useRef } from "react";
+import { useNavigate, Link } from "react-router-dom";
 import axios from "axios";
 
 function SearchBar() {
@@ -8,9 +9,53 @@ function SearchBar() {
 	const [suggestions, setSuggestions] = useState({ people: [], articles: [] });
 	const [toggleSuggestions, setToggleSuggestions] = useState(false);
 	const [highlightIndex, setHighlightIndex] = useState(-1);
+
 	const suggestionRef = useRef(null);
+	const navigate = useNavigate();
+
+	function handleEnter() {
+		if (highlightIndex !== -1) {
+			const totalPeople = suggestions.people.length;
+			if (highlightIndex < totalPeople) {
+				const user = suggestions.people[highlightIndex];
+				navigate(`/user/${user.username}-${user._id}`);
+			} else {
+				const article = suggestions.articles[highlightIndex - totalPeople];
+				navigate(
+					`/article/${article.title
+						.toLowerCase()
+						.replace(/[^a-zA-Z0-9-]/g, "")}-${article._id}`,
+				);
+			}
+			setToggleSuggestions(false);
+			setHighlightIndex(-1);
+		}
+	}
+
+	function handleKeyDown(e) {
+		if (e.key === "Escape") {
+			setToggleSuggestions(false);
+			setHighlightIndex(-1);
+		} else if (e.key === "ArrowUp" || e.key === "ArrowDown") {
+			e.preventDefault();
+			setHighlightIndex((prevIndex) => {
+				const totalSuggestions =
+					suggestions.people.length + suggestions.articles.length;
+				if (e.key === "ArrowUp") {
+					return prevIndex > 0 ? prevIndex - 1 : totalSuggestions - 1;
+				} else {
+					return (prevIndex + 1) % totalSuggestions;
+				}
+			});
+		} else if (e.key === "Enter") {
+			e.preventDefault();
+			handleEnter();
+		}
+	}
 
 	useEffect(() => {
+		setHighlightIndex(0);
+
 		const closeSuggestions = (e) => {
 			if (suggestionRef.current && !suggestionRef.current.contains(e.target)) {
 				setToggleSuggestions(false);
@@ -47,36 +92,10 @@ function SearchBar() {
 			}
 		}, 350);
 
-		const handleEscapeKey = (e) => {
-			if (e.key === "Escape") {
-				setToggleSuggestions(false);
-				setHighlightIndex(-1);
-			}
-		};
-
-		const handleArrowKeys = (e) => {
-			if (e.key === "ArrowUp" || e.key === "ArrowDown") {
-				e.preventDefault();
-				setHighlightIndex((prevIndex) => {
-					const totalSuggestions =
-						suggestions.people.length + suggestions.articles.length;
-					if (e.key === "ArrowUp") {
-						return prevIndex > 0 ? prevIndex - 1 : totalSuggestions - 1;
-					} else {
-						return (prevIndex + 1) % totalSuggestions;
-					}
-				});
-			}
-		};
-
 		window.addEventListener("click", closeSuggestions);
-		window.addEventListener("keydown", handleEscapeKey);
-		window.addEventListener("keydown", handleArrowKeys);
 
 		return () => {
 			window.removeEventListener("click", closeSuggestions);
-			window.removeEventListener("keydown", handleEscapeKey);
-			window.removeEventListener("keydown", handleArrowKeys);
 			clearTimeout(timer);
 		};
 	}, [
@@ -99,7 +118,7 @@ function SearchBar() {
 				placeholder="Search"
 				className="text-center bg-slate-100 p-1 rounded-lg outline-none outline-violet-700 outline-2 w-full"
 				onChange={handleChange}
-				onKeyDown={handleChange}
+				onKeyDown={handleKeyDown}
 				value={search}
 			/>
 
@@ -115,18 +134,24 @@ function SearchBar() {
 					<hr className="border-gray-300" />
 					{suggestions.people.length ? (
 						suggestions.people.map((person, index) => (
-							<div
+							<Link
+								to={`/user/${person.username}-${person._id}`}
 								key={person._id}
-								className={`flex mt-2 justify-center items-center hover:bg-slate-200 ${highlightIndex === index ? "bg-slate-200" : ""
-									}`}
 							>
-								<img
-									src={person.profilePicture}
-									alt="profile-pic"
-									className="rounded-full mr-4 w-8 h-8"
-								/>
-								<p className="font-medium text-slate-500">{person.username}</p>
-							</div>
+								<div
+									className={`flex mt-2 justify-center items-center hover:bg-slate-200 ${highlightIndex === index ? "bg-slate-200" : ""
+										}`}
+								>
+									<img
+										src={person.profilePicture}
+										alt="profile-pic"
+										className="rounded-full mr-4 w-8 h-8"
+									/>
+									<p className="font-medium text-slate-500">
+										{person.username}
+									</p>
+								</div>
+							</Link>
 						))
 					) : (
 						<p className="flex mt-2 justify-center text-slate-400">
@@ -138,20 +163,24 @@ function SearchBar() {
 					<hr className="border-gray-300" />
 					{suggestions.articles.length ? (
 						suggestions.articles.map((article, index) => (
-							<div
+							<Link
+								to={`/article/${article.title.toLowerCase().replace(/[^a-zA-Z0-9-]/g, "")}-${article._id}`}
 								key={article._id}
-								className={`flex mt-2 justify-center items-center hover:bg-slate-200 ${highlightIndex === suggestions.people.length + index
-										? "bg-slate-200"
-										: ""
-									}`}
 							>
-								<img
-									src={article.coverImage}
-									alt="cover-img"
-									className="rounded-lg mr-4 w-8 h-8"
-								/>
-								<p className="font-medium text-slate-500">{article.title}</p>
-							</div>
+								<div
+									className={`flex mt-2 justify-center items-center hover:bg-slate-200 ${highlightIndex === suggestions.people.length + index
+											? "bg-slate-200"
+											: ""
+										}`}
+								>
+									<img
+										src={article.coverImage}
+										alt="cover-img"
+										className="rounded-lg mr-4 w-8 h-8"
+									/>
+									<p className="font-medium text-slate-500">{article.title}</p>
+								</div>
+							</Link>
 						))
 					) : (
 						<p className="flex mt-2 justify-center text-slate-400">
--- frontend/src/components/Tags.jsx
+++ frontend/src/components/Tags.jsx
@@ -2,146 +2,145 @@ import { useState, useEffect, useRef } from "react";
 import axios from "axios";
 
 function Tags({ handleTagSelection }) {
-  const mode = import.meta.env.VITE_MODE;
+	const mode = import.meta.env.VITE_MODE;
 
-  const [value, setValue] = useState("");
-  const [suggestions, setSuggestions] = useState({ tags: [] });
-  const [toggleSuggestions, setToggleSuggestions] = useState(false);
-  const [highlightIndex, setHighlightIndex] = useState(-1);
-  const suggestionRef = useRef(null);
+	const [value, setValue] = useState("");
+	const [suggestions, setSuggestions] = useState({ tags: [] });
+	const [toggleSuggestions, setToggleSuggestions] = useState(false);
+	const [highlightIndex, setHighlightIndex] = useState(-1);
+	const suggestionRef = useRef(null);
 
-  function handleChange(e) {
-    setValue(e.target.value);
-    setToggleSuggestions(e.target.value.trim() !== "");
-    setHighlightIndex(-1);
+	function handleChange(e) {
+		setValue(e.target.value);
+		setToggleSuggestions(e.target.value.trim() !== "");
+		setHighlightIndex(-1);
 
-    if (e.target.value.trim() === "") {
-      handleTagSelection("");
-    }
-  }
+		if (e.target.value.trim() === "") {
+			handleTagSelection("");
+		}
+	}
 
-  function handleKeyDown(e) {
-    if (e.key === "Escape") {
-      setToggleSuggestions(false);
-      setHighlightIndex(-1);
-    } else if (e.key === "ArrowUp" || e.key === "ArrowDown") {
-      e.preventDefault();
-      setToggleSuggestions(true);
-    } else if (e.key === "Enter") {
-      if (highlightIndex !== -1 && suggestions.tags.length > 0) {
-        handleTagClick(suggestions.tags[highlightIndex]);
-      }
-    }
-  }
+	function handleKeyDown(e) {
+		if (e.key === "Escape") {
+			setToggleSuggestions(false);
+			setHighlightIndex(-1);
+		} else if (e.key === "ArrowUp" || e.key === "ArrowDown") {
+			e.preventDefault();
+			setToggleSuggestions(true);
+		} else if (e.key === "Enter") {
+			if (highlightIndex !== -1 && suggestions.tags.length > 0) {
+				handleTagClick(suggestions.tags[highlightIndex]);
+			}
+		}
+	}
 
-  function handleTagClick(tag) {
-    handleTagSelection(tag);
-    setToggleSuggestions(false);
-  }
+	function handleTagClick(tag) {
+		handleTagSelection(tag);
+		setToggleSuggestions(false);
+	}
 
-  useEffect(() => {
-    const closeSuggestions = (e) => {
-      if (suggestionRef.current && !suggestionRef.current.contains(e.target)) {
-        setToggleSuggestions(false);
-      }
-    };
+	useEffect(() => {
+		const closeSuggestions = (e) => {
+			if (suggestionRef.current && !suggestionRef.current.contains(e.target)) {
+				setToggleSuggestions(false);
+			}
+		};
 
-    const fetchSuggestions = async () => {
-      try {
-        const tags = await axios.get(
-          mode === "DEV"
-            ? `/api/articles/tags?query=${value}`
-            : `https://tech-tales-api.vercel.app/api/articles/tags?query=${value}`,
-          { withCredentials: true },
-        );
-        setSuggestions({ tags: tags.data });
-      } catch (error) {
-        error.message = error.response
-          ? error.response.data.message
-          : error.response.statusText;
-        console.log(error.message);
-      }
-    };
+		const fetchSuggestions = async () => {
+			try {
+				const tags = await axios.get(
+					mode === "DEV"
+						? `/api/articles/tags?query=${value}`
+						: `https://tech-tales-api.vercel.app/api/articles/tags?query=${value}`,
+					{ withCredentials: true },
+				);
+				setSuggestions({ tags: tags.data });
+			} catch (error) {
+				error.message = error.response
+					? error.response.data.message
+					: error.response.statusText;
+				console.log(error.message);
+			}
+		};
 
-    const timer = setTimeout(() => {
-      if (value.trim() !== "") {
-        fetchSuggestions();
-      } else {
-        setSuggestions({ tags: [] });
-      }
-    }, 350);
+		const timer = setTimeout(() => {
+			if (value.trim() !== "") {
+				fetchSuggestions();
+			} else {
+				setSuggestions({ tags: [] });
+			}
+		}, 350);
 
-    const handleEscapeKey = (e) => {
-      if (e.key === "Escape") {
-        setToggleSuggestions(false);
-        setHighlightIndex(-1);
-      }
-    };
+		const handleEscapeKey = (e) => {
+			if (e.key === "Escape") {
+				setToggleSuggestions(false);
+				setHighlightIndex(-1);
+			}
+		};
 
-    const handleArrowKeys = (e) => {
-      if (e.key === "ArrowUp" || e.key === "ArrowDown") {
-        e.preventDefault();
-        setHighlightIndex((prevIndex) => {
-          const totalSuggestions = suggestions.tags.length;
-          if (e.key === "ArrowUp") {
-            return prevIndex > 0 ? prevIndex - 1 : totalSuggestions - 1;
-          } else {
-            return (prevIndex + 1) % totalSuggestions;
-          }
-        });
-      }
-    };
+		const handleArrowKeys = (e) => {
+			if (e.key === "ArrowUp" || e.key === "ArrowDown") {
+				e.preventDefault();
+				setHighlightIndex((prevIndex) => {
+					const totalSuggestions = suggestions.tags.length;
+					if (e.key === "ArrowUp") {
+						return prevIndex > 0 ? prevIndex - 1 : totalSuggestions - 1;
+					} else {
+						return (prevIndex + 1) % totalSuggestions;
+					}
+				});
+			}
+		};
 
-    window.addEventListener("click", closeSuggestions);
-    window.addEventListener("keydown", handleEscapeKey);
-    window.addEventListener("keydown", handleArrowKeys);
+		window.addEventListener("click", closeSuggestions);
+		window.addEventListener("keydown", handleEscapeKey);
+		window.addEventListener("keydown", handleArrowKeys);
 
-    return () => {
-      window.removeEventListener("click", closeSuggestions);
-      window.removeEventListener("keydown", handleEscapeKey);
-      window.removeEventListener("keydown", handleArrowKeys);
-      clearTimeout(timer);
-    };
-  }, [toggleSuggestions, value, suggestions.tags.length]);
+		return () => {
+			window.removeEventListener("click", closeSuggestions);
+			window.removeEventListener("keydown", handleEscapeKey);
+			window.removeEventListener("keydown", handleArrowKeys);
+			clearTimeout(timer);
+		};
+	}, [toggleSuggestions, value, suggestions.tags.length]);
 
-  return (
-    <div className="relative self-center w-full md:w-1/4 mx-auto mt-2">
-      <input
-        type="text"
-        id="search_tags"
-        placeholder="Enter tags to filter"
-        onChange={handleChange}
-        onKeyDown={handleKeyDown}
-        value={value}
-        className="text-center bg-slate-100 p-1 rounded-lg outline-none outline-violet-700 outline-2 w-full"
-      />
+	return (
+		<div className="relative self-center w-full md:w-1/4 mx-auto mt-2">
+			<input
+				type="text"
+				id="search_tags"
+				placeholder="Enter tags to filter"
+				onChange={handleChange}
+				onKeyDown={handleKeyDown}
+				value={value}
+				className="text-center bg-slate-100 p-1 rounded-lg outline-none outline-violet-700 outline-2 w-full"
+			/>
 
-      {toggleSuggestions && (
-        <div
-          className="absolute z-10 w-full bg-slate-100 p-2 mt-2 rounded-lg shadow-lg overflow-auto"
-          ref={suggestionRef}
-        >
-          {suggestions.tags.length > 0 ? (
-            suggestions.tags.map((tag, index) => (
-              <div
-                key={index}
-                onClick={() => handleTagClick(tag)}
-                className={`flex justify-center items-center hover:bg-slate-200 ${
-                  highlightIndex === index ? "bg-slate-200" : ""
-                }`}
-              >
-                <p className="font-medium text-slate-500">{tag}</p>
-              </div>
-            ))
-          ) : (
-            <p className="flex mt-2 justify-center text-slate-400">
-              No tags found
-            </p>
-          )}
-        </div>
-      )}
-    </div>
-  );
+			{toggleSuggestions && (
+				<div
+					className="absolute z-10 w-full bg-slate-100 p-2 mt-2 rounded-lg shadow-lg overflow-auto"
+					ref={suggestionRef}
+				>
+					{suggestions.tags.length > 0 ? (
+						suggestions.tags.map((tag, index) => (
+							<div
+								key={index}
+								onClick={() => handleTagClick(tag)}
+								className={`flex justify-center items-center hover:bg-slate-200 ${highlightIndex === index ? "bg-slate-200" : ""
+									}`}
+							>
+								<p className="font-medium text-slate-500">{tag}</p>
+							</div>
+						))
+					) : (
+						<p className="flex mt-2 justify-center text-slate-400">
+							No tags found
+						</p>
+					)}
+				</div>
+			)}
+		</div>
+	);
 }
 
 export default Tags;
